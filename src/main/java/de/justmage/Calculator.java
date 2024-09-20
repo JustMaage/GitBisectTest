be77a060354a555baba0b13ba6a306dd783aae19
JustMage <justmagemc@gmail.com>
2024-09-20 18:32:59 +0200

    added git bisect comment

--- src/main/java/de/justmage/Calculator.java
+++ src/main/java/de/justmage/Calculator.java
@@ -2,6 +2,9 @@ package de.justmage;
 
 public class Calculator {
 
+    // this method no longer works
+    // try using "git bisect" to find the faulty commit
+    // (this is fairly easy but still :)
     public int add(int a, int b) {
         return a + b + 1;
     }
